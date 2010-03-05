b8caa387cbb67cfd113d5ac589217cfb0cf9f42f
Tristan Davies <tristan@tristandavies.me>
2010-03-04 19:36:20 -0500

    Moved autoload function from index.php to system/autoload.php

--- index.php
+++ index.php
@@ -1,59 +1,7 @@
 <?php
 date_default_timezone_set(@date_default_timezone_get()); //timezone E_STRICT annoying shitfix.
 
-function __autoload($class)
-	{
-	$checked = array();
-	$folders = array('system/libraries','system/core','application/controllers','application/models','system/helpers');
-	if (defined('CONFIG_LOADED'))
-		{
-		$folders = (s('config')->autoload_folders) ? s('config')->autoload_folders : $folders;
-		$predefined = (s('config')->class_locations) ? s('config')->class_locations : array();
-		if (s('config')->compatibility['enabled'])
-			{
-			$folders[] = 'system/compatibility/'.$class;
-			foreach (s('config')->compatibility['class_prefixes'] as $prefix => $class_name)
-				{
-				if (substr($class,0,strlen($prefix)) == $prefix)
-					{
-					$folders = array();
-					foreach (s('config')->compatibility['autoload_folders'][$class_name] as $folder)
-						{
-						$folders[] = DIR_COMPATIBILITY.$class_name.'/'.$folder;
-						}
-					$file_noprefix = substr($class,strlen($prefix));
-					break;
-					}
-				}
-			}
-		}
-	if (isset($predefined[$class]))
-		{
-		$folders = array($predefined[$class]);
-		}
-	foreach ($folders as $folder)
-		{
-		$file = $folder.'/'.((isset($file_noprefix)) ? $file_noprefix :$class).'.php';
-		$checked[] = $file;
-		if (file_exists($file))
-			{
-			require_once($file);
-			break;
-			}
-		}
-	if (!class_exists($class))
-		{
-		echo "Couldn't find class $class<br/>\n";
-		echo "Checked the following locations:<br/>\n";
-		echo "<ul>\n";
-		foreach ($checked as $file)
-			{
-			echo "<li>$file</li>\n";
-			}
-		echo "</ul>";
-		die();
-		}
-	}
+require_once('system/autoload.php');
 
 //some shortcuts
 function s($class) //stands for singleton
@@ -77,4 +25,4 @@ s('damien')->define_constants();
 
 s('event')->trigger('initialize');
 
-//continue here
+
