2aefcaf9e9eceac3dbf9d82b70a025f018f5347c
Tristan Davies <tristan@tristandavies.me>
2010-03-04 22:15:16 -0500

    overhead timing

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+error_reporting(E_ALL);
+$overhead_start = microtime(true);
 require_once('system/autoload.php');
-
 //some shortcuts
 function s($class) //stands for singleton
 	{
@@ -16,13 +17,13 @@ foreach (s('config')->autoload_classes as $class)
 	s($class);
 	}
 unset($class);
-
 if (!s('config')->enabled)
 	{
 	s('event')->trigger('site_offline');
 	}
-
-s('timing')->play('total');
+s('timing')->play('total')->set('total',microtime(true)-$overhead_start);
+s('timing')->pause('overhead')->set('overhead',microtime(true)-$overhead_start);
 s('event')->trigger('initialize');
 
-
+sleep(1);
+echo '<pre>'.print_r(s('timing')->table(),true).'</pre>';
