9f0a7bafd24e581f285a2e5ba2de8c4ab656598a
Tristan Davies <tristan@tristandavies.me>
2010-03-04 19:28:02 -0500

    Moved stuff to CI.php

--- system/compatibility/codeigniter/codeigniter.php
+++ system/compatibility/codeigniter/codeigniter.php
@@ -35,21 +35,3 @@ class codeigniter extends helper {
 		return s('CI_'.ucfirst($name));
 		}
 }
-class codeigniter_language {
-private $lang;
-private $loaded;
-	public function load($file)
-		{
-		if (!isset($this->loaded[$file]))
-			{
-			$this->loaded[$file] = false;
-			$lang = $this->lang;
-			include(DIR_COMPATIBILITY.'codeigniter/language/'.s('config')->language.'/'.$file.'_lang.php');
-			$this->lang = $lang;
-			}
-		}
-	public function line($field)
-		{
-		return (isset($this->lang[$field])) ? $this->lang[$field] : $field;
-		}
-}
