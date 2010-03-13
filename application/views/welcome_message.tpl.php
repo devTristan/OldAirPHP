123a48cb56bf6c4cfc9b942659bdc07fa3b0bcd7
Tristan Davies <tristan@tristandavies.me>
2010-03-13 12:40:33 -0500

    Uses external (minified) css file.

--- application/views/welcome_message.tpl.php
+++ application/views/welcome_message.tpl.php
@@ -1,47 +1,7 @@
 <html>
 <head>
 <title>Welcome to <?php echo s('config')->framework_name.' '.s('config')->framework_version; ?></title>
-
-<style type="text/css">
-
-body {
- background-color: #fff;
- margin: 40px;
- font-family: Lucida Grande, Verdana, Sans-serif;
- font-size: 14px;
- color: #4F5155;
-}
-
-a {
- color: #003399;
- background-color: transparent;
- font-weight: normal;
-}
-
-h1 {
- color: #444;
- background-color: transparent;
- border-bottom: 1px solid #D0D0D0;
- font-size: 16px;
- font-weight: bold;
- margin: 24px 0 2px 0;
- padding: 5px 0 6px 0;
-}
-
-code, pre {
- font-size: 12px;
- background-color: #f9f9f9;
- border: 1px solid #D0D0D0;
- color: #002166;
- display: block;
- margin: 14px 0 14px 0;
- padding: 12px 10px 12px 10px;
-}
-
-code {
- font-family: Monaco, Verdana, Sans-serif;
-}
-</style>
+<link rel="stylesheet" href="<?php echo URL_BASE.'welcome.css'; ?>" type="text/css" media="screen" charset="utf-8" />
 </head>
 <body>
 
