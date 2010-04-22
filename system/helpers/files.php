3c000a4389875faa04d8a388f502eecb6d4575a1
devTristan <devTristan@gmail.com>
2010-04-22 19:37:51 -0400

    Made the readable_bytes function better by making it recognize sizes above 1000 TB

--- system/helpers/files.php
+++ system/helpers/files.php
@@ -16,11 +16,11 @@ class files extends helper {
 		}
 	static public function readable_bytes($bytes, $precision = 2)
 		{
-		$units = array('B', 'KB', 'MB', 'GB', 'TB');
+		$units = array('','K','M','G','T','P','E','Z','Y');
 		$bytes = max($bytes, 0);
 		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
 		$pow = min($pow, count($units) - 1);
 		$bytes /= pow(1024, $pow);
-		return round($bytes, $precision) . ' ' . $units[$pow];
+		return round($bytes, $precision) . ' ' . $units[$pow].'B';
 		}
 }
