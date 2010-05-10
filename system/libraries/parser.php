9880e50cc4862da7ff2aa94b255d050443528682
devTristan <devTristan@gmail.com>
2010-05-09 23:30:39 -0400

    Changed parsefile method to let the developer specify filters instead of reading them from the filename

--- system/libraries/parser.php
+++ system/libraries/parser.php
@@ -8,11 +8,18 @@ class parser extends library {
 			}
 		return $data;
 		}
-	public function parsefile($file)
+	public function parsefile($file,$filters = array())
 		{
-		$filters = explode('.',substr($file,strrpos($file,'/')+1));
-		array_shift($filters);
-		$ext = array_pop($filters);
+		if ($filters == array())
+			{
+			$filters = explode('.',substr($file,strrpos($file,'/')+1));
+			array_shift($filters);
+			$ext = array_pop($filters);
+			}
+		else
+			{
+			$ext = array_pop(explode('.',substr($file,strrpos($file,'/')+1)));
+			}
 		return (count($filters) == 0) ? false : $this->parse(file_get_contents($file),$filters,$ext);
 		}
 }
